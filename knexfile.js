ea03bf083ca6f1c6b22c23f53b527f5ac648acf3
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-09-05 18:03:26 -0500

    Sanity Commit

--- knexfile.js
+++ knexfile.js
@@ -15,3 +15,20 @@ module.exports = {
     seeds: { directory: './data/seeds' },
   },
 };
+module.exports = {
+  production: {
+    client: 'sqlite3',
+    connection: { filename: './data/lambda.db3' },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+};
